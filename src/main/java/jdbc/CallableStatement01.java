2d1aacaa4014b5a0bb32dd191230e9d0eaee7a86
Sümeyye Geçici <sumeyyegecici1998@gmail.com>
2023-06-04 20:01:55 +0300

    JDBC

--- src/main/java/jdbc/CallableStatement01.java
+++ src/main/java/jdbc/CallableStatement01.java
@@ -17,7 +17,7 @@ public class CallableStatement01 {
 
         //1. Example: İki parametre ile calisip bu parametreleri toplayarak return yapan bir fonksiyon olusturun
 
-        // 1. Adim: Fonksiyon kodunu yaz
+        // 1. Step: Fonksiyon kodunu yaz
         String sql1 = "CREATE OR REPLACE FUNCTION toplamaF(x NUMERIC, y NUMERIC)\n" +
                 "RETURNS NUMERIC\n" +
                 "LANGUAGE plpgsql\n" +
@@ -30,23 +30,23 @@ public class CallableStatement01 {
                 " END\n" +
                 "$$";
 
-        // 2. Adim: Fonksiyonu calistir
+        // 2. Step: Fonksiyonu calistir
         st.execute(sql1);
 
-        // 3. Adim: Fonksiyonu cagir
+        // 3. Step: Fonksiyonu cagir
         CallableStatement cst1 = con.prepareCall("{? = call toplamaF(?, ?)}"); // bu fonksiyonu bana şu parametrelerle cagir dedik
                                                     // functional cagirma syntax'i
 
 
-        // 4. Adim: Return icin registerOutParameter() methodunu, parametreler icin set() methodlarindan uygun olanlari kullan
+        // 4. Step: Return icin registerOutParameter() methodunu, parametreler icin set() methodlarindan uygun olanlari kullan
         cst1.registerOutParameter(1, Types.NUMERIC);
         cst1.setInt(2, 15);
         cst1.setInt(3, 25);
 
-        // 5. Adim: Calistirmak icin execute() methodu kullan
+        // 5. Step: Calistirmak icin execute() methodu kullan
         cst1.execute();
 
-        // 6. Adim: Sonucu cagırmak icin return data tipine gore "get" methodlarindan uygun olani kullan
+        // 6. Step: Sonucu cagırmak icin return data tipine gore "get" methodlarindan uygun olani kullan
         System.out.println(cst1.getBigDecimal(1));
 
 
@@ -56,7 +56,7 @@ public class CallableStatement01 {
 
         // 2. Example: Koninin hacmini hesaplayan bir function yazin
 
-        // 1. Adim: Fonksiyon kodunu yaz
+        // 1. Step: Fonksiyon kodunu yaz
         String sql2 = "CREATE OR REPLACE FUNCTION koniHacmi(r NUMERIC, h NUMERIC)\n" +
                 "RETURNS NUMERIC\n" +
                 "LANGUAGE plpgsql\n" +
@@ -70,21 +70,21 @@ public class CallableStatement01 {
                 "$$";
 
 
-        // 2. Adim: Fonksiyonu calistir
+        // 2. Step: Fonksiyonu calistir
         st.execute(sql2);
 
-        // 3. Adim: Fonksiyonu cagir
+        // 3. Step: Fonksiyonu cagir
         CallableStatement cst2 = con.prepareCall("{? = call koniHacmi(?, ?)}");
 
-        // 4. Adim: Return icin registerOutParameter() methodunu, parametreler icin set() methodlarindan uygun olanlari kullan
+        // 4. Step: Return icin registerOutParameter() methodunu, parametreler icin set() methodlarindan uygun olanlari kullan
         cst2.registerOutParameter(1, Types.NUMERIC);
         cst2.setInt(2,1);
         cst2.setInt(3,1);
 
-        // 5. Adim: Calistirmak icin execute() methodu kullan
+        // 5. Step: Calistirmak icin execute() methodu kullan
         cst2.execute();
 
-        // 6. Adim: Sonucu cagırmak icin return data tipine gore "get" methodlarindan uygun olani kullan
+        // 6. Step: Sonucu cagırmak icin return data tipine gore "get" methodlarindan uygun olani kullan
         System.out.println(cst2.getBigDecimal(1));
 
 
